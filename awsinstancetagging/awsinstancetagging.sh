b55be5f7eabbd9aa50873bb00f7009fe4d5faee7
Saikumar Vattem <saikumar.vattem@metservice.com>
2022-10-19 12:21:52 +1300

    deleted

--- awsinstancetagging/awsinstancetagging.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-export AWS_PROFILE=$1
-echo "Profile configured as - $AWS_PROFILE"
-while IFS=, read -r instance region tagvalue; do
-	echo "Adding/Updating $instance in location $region with Environment tag as $tagvalue"
-	ansible-playbook /home/adm-svattem/scripts/awsinstancetagging/awsinstancetagging.yaml --extra-vars="instanceid=$instance region=$region tagvalue=$tagvalue"
-done < /home/adm-svattem/scripts/awsinstancetagging/hosts.txt
